52fd621b9c0efdb48738e9cb024f381c44b34f93
Natasha Colusso <natashacolusso@gmail.com>
2020-12-12 01:17:51 +0000

    completed all core function on vampire object

--- vampire.js
+++ vampire.js
@@ -6,26 +6,35 @@ class Vampire {
     this.creator = null;
   }
 
+
+
   /** Simple tree methods **/
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let numFromTop = 0;
+    let currentVamp = this;
+    while (currentVamp.creator) {
+      currentVamp = currentVamp.creator;
+      numFromTop ++;
+    }
+    return numFromTop;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return vampire.numberOfVampiresFromOriginal > this.numberOfVampiresFromOriginal
   }
 
   /** Stretch **/
@@ -36,6 +45,7 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+
 
   }
 }
